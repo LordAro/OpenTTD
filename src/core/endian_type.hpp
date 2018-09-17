3f5e59b7130d1dc6c9644e6d135ebf0a1a2f2611
Charles Pigott <charlespigott@googlemail.com>
2018-09-17 17:05:43 +0100

    Use CMake to determine endianness

--- src/core/endian_type.hpp
+++ src/core/endian_type.hpp
@@ -25,16 +25,4 @@
 /** Big endian builds use this for TTD_ENDIAN. */
 #define TTD_BIG_ENDIAN 1
 
-/* Windows has always LITTLE_ENDIAN */
-#if defined(WIN32) || defined(__OS2__) || defined(WIN64)
-	#define TTD_ENDIAN TTD_LITTLE_ENDIAN
-#elif !defined(TESTING)
-	/* Else include endian[target/host].h, which has the endian-type, autodetected by the Makefile */
-	#if defined(STRGEN) || defined(SETTINGSGEN)
-		#include "endian_host.h"
-	#else
-		#include "endian_target.h"
-	#endif
-#endif /* WIN32 || __OS2__ || WIN64 */
-
 #endif /* ENDIAN_TYPE_HPP */
