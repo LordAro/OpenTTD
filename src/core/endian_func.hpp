JGRennison/OpenTTD-patches

Add 64 bit byte swapping function

src/core/endian_func.hpp
@@ -19,25 +19,33 @@
 #if TTD_ENDIAN == TTD_BIG_ENDIAN
 	#define FROM_BE16(x) (x)
 	#define FROM_BE32(x) (x)
+	#define FROM_BE64(x) (x)
 	#define TO_BE16(x)   (x)
 	#define TO_BE32(x)   (x)
 	#define TO_BE32X(x)  (x)
+	#define TO_BE64(x)   (x)
 	#define FROM_LE16(x) BSWAP16(x)
 	#define FROM_LE32(x) BSWAP32(x)
+	#define FROM_LE64(x) BSWAP64(x)
 	#define TO_LE16(x)   BSWAP16(x)
 	#define TO_LE32(x)   BSWAP32(x)
 	#define TO_LE32X(x)  BSWAP32(x)
+	#define TO_LE64(x)   BSWAP64(x)
 #else
 	#define FROM_BE16(x) BSWAP16(x)
 	#define FROM_BE32(x) BSWAP32(x)
+	#define FROM_BE64(x) BSWAP64(x)
 	#define TO_BE16(x)   BSWAP16(x)
 	#define TO_BE32(x)   BSWAP32(x)
 	#define TO_BE32X(x)  BSWAP32(x)
+	#define TO_BE64(x)   BSWAP64(x)
 	#define FROM_LE16(x) (x)
 	#define FROM_LE32(x) (x)
+	#define FROM_LE64(x) (x)
 	#define TO_LE16(x)   (x)
 	#define TO_LE32(x)   (x)
 	#define TO_LE32X(x)  (x)
+	#define TO_LE64(x)   (x)
 #endif /* TTD_ENDIAN == TTD_BIG_ENDIAN */
 
 static inline uint16 ReadLE16Aligned(const void *x)

LordAro/OpenTTD

src/core/endian_func.hpp
@@ -17,25 +17,33 @@
 #if TTD_ENDIAN == TTD_BIG_ENDIAN
 #	define FROM_BE16(x) (x)
 #	define FROM_BE32(x) (x)
+#	define FROM_BE64(x) (x)
 #	define TO_BE16(x)   (x)
 #	define TO_BE32(x)   (x)
 #	define TO_BE32X(x)  (x)
+#	define TO_BE64(x)   (x)
 #	define FROM_LE16(x) BSWAP16(x)
 #	define FROM_LE32(x) BSWAP32(x)
+#	define FROM_LE64(x) BSWAP64(x)
 #	define TO_LE16(x)   BSWAP16(x)
 #	define TO_LE32(x)   BSWAP32(x)
 #	define TO_LE32X(x)  BSWAP32(x)
+#	define TO_LE64(x)   BSWAP64(x)
 #else
 #	define FROM_BE16(x) BSWAP16(x)
 #	define FROM_BE32(x) BSWAP32(x)
+#	define FROM_BE64(x) BSWAP64(x)
 #	define TO_BE16(x)   BSWAP16(x)
 #	define TO_BE32(x)   BSWAP32(x)
 #	define TO_BE32X(x)  BSWAP32(x)
+#	define TO_BE64(x)   BSWAP64(x)
 #	define FROM_LE16(x) (x)
 #	define FROM_LE32(x) (x)
+#	define FROM_LE64(x) (x)
 #	define TO_LE16(x)   (x)
 #	define TO_LE32(x)   (x)
 #	define TO_LE32X(x)  (x)
+#	define TO_LE64(x)   (x)
 #endif /* TTD_ENDIAN == TTD_BIG_ENDIAN */
 
 static inline uint16 ReadLE16Aligned(const void *x)